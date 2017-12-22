e90872f0364fe1ac27b99ea313e90dd49efbc1bd
hekrxe <hekrxe@163.com>
2017-12-22 16:22:01 +0800

    apis

--- common/src/main/scala/com/arkr/common/zk/ZKClient.scala
+++ common/src/main/scala/com/arkr/common/zk/ZKClient.scala
@@ -7,7 +7,6 @@ import com.arkr.common.exception.ArkrRuntimeException
 import org.apache.curator.framework.recipes.cache.{NodeCache, PathChildrenCache, PathChildrenCacheListener}
 import org.apache.curator.framework.{CuratorFramework, CuratorFrameworkFactory}
 import org.apache.curator.retry.ExponentialBackoffRetry
-import org.apache.curator.utils.{PathUtils, ZKPaths}
 import org.apache.zookeeper.CreateMode
 import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.core.io.support.PropertiesLoaderUtils
@@ -16,63 +15,56 @@ import org.springframework.core.io.support.PropertiesLoaderUtils
   * @author hztanhuayou
   * @date 2017/12/21
   */
-private class ZKClient {
+private class ZKClient private {
   private val DEF_CHARSET = Charset.forName("UTF-8")
   private val EMPTY_DATA = "" getBytes DEF_CHARSET
   private val CLIENT = init()
-  private var APP_NAMESPACE: String = _
 
   @throws[Exception]
   def getObject[T](path: String, clazz: Class[T]): T = {
-    val appPath = makePath(path)
-    checkPath(appPath)
+    checkPath(path)
 
-    val bytes = CLIENT.getData.forPath(appPath)
+    val bytes = CLIENT.getData.forPath(path)
     JSON.parseObject(bytes, 0, bytes.length, DEF_CHARSET, clazz)
   }
 
   @throws[Exception]
   def setData(path: String, data: Object): Unit = {
-    val appPath = makePath(path)
-    checkPath(appPath)
+    checkPath(path)
     notNull(data)
 
-    CLIENT.setData().forPath(appPath, JSON.toJSONString(data, false).getBytes(DEF_CHARSET))
+    CLIENT.setData().forPath(path, JSON.toJSONString(data, false).getBytes(DEF_CHARSET))
   }
 
   @throws[Exception]
   def create(path: String, data: Object, createMode: CreateMode = CreateMode.PERSISTENT): Unit = {
-    val appPath = makePath(path)
-    if (!exist(appPath)) {
+    if (!exist(path)) {
       val dataBytes = if (null == data) {
         EMPTY_DATA
       } else {
         JSON.toJSONString(data, false).getBytes(DEF_CHARSET)
       }
-      CLIENT.create().creatingParentsIfNeeded().withMode(createMode).forPath(appPath, dataBytes)
+      CLIENT.create().creatingParentsIfNeeded().withMode(createMode).forPath(path, dataBytes)
     }
   }
 
   @throws[Exception]
   def delete(path: String): Unit = {
-    val appPath = makePath(path)
-    if (exist(appPath)) {
-      CLIENT.delete().guaranteed().forPath(appPath)
+    if (exist(path)) {
+      CLIENT.delete().guaranteed().forPath(path)
     }
   }
 
   @throws[Exception]
   def deleteIfChildrenNeeded(path: String): Unit = {
-    val appPath = makePath(path)
-    if (exist(appPath)) {
-      CLIENT.delete().guaranteed().deletingChildrenIfNeeded().forPath(appPath)
+    if (exist(path)) {
+      CLIENT.delete().guaranteed().deletingChildrenIfNeeded().forPath(path)
     }
   }
 
   @throws[Exception]
   def pathWatcher(path: String, listener: NodeListener): Unit = {
-    val appPath = makePath(path)
-    val nodeCache = new NodeCache(CLIENT, appPath, false)
+    val nodeCache = new NodeCache(CLIENT, path, false)
     listener.setNodeCache(nodeCache)
     nodeCache.getListenable.addListener(listener)
     nodeCache.start(true)
@@ -80,8 +72,7 @@ private class ZKClient {
 
   @throws[Exception]
   def childrenPathWatcher(path: String, listener: PathChildrenCacheListener): Unit = {
-    val appPath = makePath(path)
-    val cache = new PathChildrenCache(CLIENT, appPath, true)
+    val cache = new PathChildrenCache(CLIENT, path, true)
     cache.getListenable.addListener(listener)
     cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT)
   }
@@ -104,17 +95,15 @@ private class ZKClient {
     obj
   }
 
-  private def makePath(path: String): String = ZKPaths.makePath(APP_NAMESPACE, path)
-
 
   private def init(): CuratorFramework = {
     val loader = new DefaultResourceLoader()
     val resource = loader.getResource("classpath:config/zk.properties")
     val properties = PropertiesLoaderUtils.loadProperties(resource)
-    val connectString = properties.getProperty("connect.string", "localhost:2181")
+    val connectString = properties.getProperty("connect.string", "my.linux.com:2181,my.linux.com:2182")
     val namespace = properties.getProperty("namespace", "arkr")
-    val sessionTimeoutMs = properties.getProperty("session.timeout.millis", "60000").asInstanceOf[Int]
-    val connectionTimeoutMs = properties.getProperty("connection.timeout.millis", "15000").asInstanceOf[Int]
+    val sessionTimeoutMs = properties.getProperty("session.timeout.millis", "60000").toInt
+    val connectionTimeoutMs = properties.getProperty("connection.timeout.millis", "15000").toInt
 
     val client = CuratorFrameworkFactory.builder()
       .connectString(connectString)
@@ -139,9 +128,33 @@ object ZKClient {
     val ZK_NOT_NULL = 600
   }
 
-  def apply(namespace: String): ZKClient = {
-    PathUtils.validatePath(namespace)
-    client.APP_NAMESPACE = namespace
-    client
-  }
+  @throws[Exception]
+  def getObject[T](path: String, clazz: Class[T]): T = client.getObject(path, clazz)
+
+  @throws[Exception]
+  def setData(path: String, data: Object): Unit = client setData(path, data)
+
+  @throws[Exception]
+  def create(path: String, data: Object, createMode: CreateMode = CreateMode.PERSISTENT): Unit =
+    client.create(path, data, createMode)
+
+  @throws[Exception]
+  def delete(path: String): Unit = client.delete(path)
+
+  @throws[Exception]
+  def deleteIfChildrenNeeded(path: String): Unit = client.deleteIfChildrenNeeded(path)
+
+  @throws[Exception]
+  def pathWatcher(path: String, listener: NodeListener): Unit = client.pathWatcher(path, listener)
+
+  @throws[Exception]
+  def childrenPathWatcher(path: String, listener: PathChildrenCacheListener): Unit =
+    client.childrenPathWatcher(path, listener)
+
+  @throws[Exception]
+  def exist(path: String): Boolean = client.exist(path)
+}
+
+object MainApp extends App {
+  ZKClient.create("/nihao", "hao")
 }
