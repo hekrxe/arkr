1db8d2e19e1563e3abc11fec9d7da88046dae3dd
hekrxe <hekrxe@163.com>
2018-01-25 18:43:48 +0800

    kafka-producer usage

--- servers/hear/src/main/scala/com/arkr/hear/consumer/PushListener.scala
+++ servers/hear/src/main/scala/com/arkr/hear/consumer/PushListener.scala
@@ -3,7 +3,10 @@ package com.arkr.hear.consumer
 import com.alibaba.fastjson.JSON
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
 /**
@@ -12,12 +15,17 @@ import org.springframework.stereotype.Component
   */
 
 @Component("pushListener")
-class PushListener {
+class PushListener extends InitializingBean {
   private val logger = LoggerFactory.getLogger(getClass)
+
+  @Autowired private var kafkaProducerTemplate: KafkaTemplate[String, String] = _
 
   @KafkaListener(topics = Array("topic.push"))
   def receive(record: ConsumerRecord[String, String]): Unit = {
     logger.info(JSON.toJSONString(record, false))
   }
 
+  override def afterPropertiesSet(): Unit = {
+    kafkaProducerTemplate.send("topic.push", "data........")
+  }
 }
