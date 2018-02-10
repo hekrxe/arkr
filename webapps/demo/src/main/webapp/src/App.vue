552fdbb1b70bba3c2044ca9cc67579f463a4bf12
hekrxe <hekrxe@163.com>
2018-02-10 18:41:47 +0800

    spring

--- webapps/demo/src/main/webapp/src/App.vue
+++ webapps/demo/src/main/webapp/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="/">Project name</a>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="navbar-header">
+        <div class="m-logo">
+          <div class="logo"></div>
         </div>
+      </div>
+      <div class="container-fluid">
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
+            <li><a href="#">谭化友</a></li>
+            <li><a href="#">退出</a></li>
           </ul>
         </div>
       </div>
@@ -16,14 +18,14 @@
 
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
+        <div class="sidebar">
           <ul class="nav nav-sidebar">
             <li><router-link to="/about">About</router-link> </li>
             <li><router-link to="/customers">Customer</router-link></li>
             <li><router-link to="/add">Add</router-link></li>
           </ul>
         </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <div class="main">
           <router-view></router-view>
         </div>
       </div>
@@ -40,35 +42,46 @@
 </script>
 
 <style>
-  body {
-    padding-top: 50px;
+  .logo {
+    position: relative;
+    top: 12px;
+    left: 22px;
+    display: inline-block;
+    width: 88px;
+    height: 25px;
+    background: url("./assets/logo-mini.png") no-repeat;
+    background-position: left center;
+    background-size: contain;
   }
 
-  .navbar-fixed-top {
-    border: 0;
+  .m-logo {
+    position: relative;
+    width: 180px;
+    height: 54px;
+    padding: 0;
+    font-size: 12px;
+    color: #fff;
+    line-height: 30px;
+    background-color: #5294e0;
   }
 
   .sidebar {
-    display: none;
+    width: 180px;
+    position: absolute;
+    top: 54px;
+    left: 0;
+    z-index: 1000;
+    display: block;
+    padding: 20px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    background-color: #f5f5f5;
+    border-right: 1px solid #eee;
+    right: 0;
+    bottom: 0;
+    margin-bottom: 0;
   }
 
-  @media (min-width: 768px) {
-    .sidebar {
-      position: fixed;
-      top: 51px;
-      bottom: 0;
-      left: 0;
-      z-index: 1000;
-      display: block;
-      padding: 20px;
-      overflow-x: hidden;
-      overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-      background-color: #f5f5f5;
-      border-right: 1px solid #eee;
-    }
-  }
-
-  /* Sidebar navigation */
   .nav-sidebar {
     margin-right: -21px; /* 20px padding + 1px border */
     margin-bottom: 20px;
@@ -87,30 +100,8 @@
     background-color: #428bca;
   }
 
-  /*
-   * Main content
-   */
   .main {
-    padding: 10px;
-  }
-
-  @media (min-width: 768px) {
-    .main {
-      padding-right: 10px;
-      padding-left: 10px;
-    }
-  }
-
-  .main .page-header {
-    margin-top: 0;
-  }
-
-  .placeholders h4 {
-    margin-bottom: 0;
-  }
-
-  .placeholder img {
-    display: inline-block;
-    border-radius: 50%;
+    margin-left: 182px;
+    margin-top: 54px;
   }
 </style>
